a6e5e2f98044f56a35f40367a112089a81e15541
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2025-02-20 15:15:23 +0000

    Add support for "read more" box

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,4 +38,13 @@
   };
 
   $('.tracked').on('click', trackOutboundLink);
+
+  for (const jsReadMoreLink of document.getElementsByClassName('js-read-more')) {
+    jsReadMoreLink.onclick = () => {
+      const elementId = jsReadMoreLink.dataset.target;
+      const element = document.getElementById(elementId);
+      element.classList.toggle('read-more');
+      jsReadMoreLink.innerText = jsReadMoreLink.innerText === "(show)" ? "(hide)" : "(show)";
+    };
+  }
 })();
