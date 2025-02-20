57d1a10bf73e3f8eb689917a444e10d9270118fc
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2025-02-20 17:12:46 +0000

    Update script to check image first and include author as fallback in search

--- fetch-book-data.rb
+++ fetch-book-data.rb
@@ -6,7 +6,13 @@ require 'open-uri'
 outline = YAML.load_file('_data/outline.yml')
 
 def fetch_book_info(title, subtitle, author)
-  query = URI.encode_www_form_component("#{title} #{subtitle}")
+  cover_image_path = image_path(title)
+  if File.exist?(cover_image_path)
+    puts "Already have a cover image for book '#{title}' at '#{cover_image_path}', will not download again"
+    return
+  end
+
+  query = URI.encode_www_form_component("#{title} #{subtitle || author}")
   base_url = "https://openlibrary.org/search.json?q=#{query}&limit=1"
   response = Net::HTTP.get(URI(base_url))
   data = JSON.parse(response)
@@ -14,7 +20,7 @@ def fetch_book_info(title, subtitle, author)
   if data['docs'] && !data['docs'].empty?
     book = data['docs'].first
     cover_id = book['cover_i']
-    download_cover(title, cover_id)
+    download_cover(title, cover_id, cover_image_path)
   else
     puts "WARN: Did not get any data for book with title '#{title}'"
   end
@@ -28,36 +34,23 @@ def image_path(title)
   "assets/img/books/#{dasherize(title)}.jpg"
 end
 
-def download_cover(title, cover_id)
+def download_cover(title, cover_id, cover_image_path)
   unless cover_id
     puts "No cover image available for book '#{title}'"
     return
   end
 
-  file_path = image_path(title)
-  if File.exist?(file_path)
-    puts "Cover image for book '#{title}' already exists at '#{file_path}', so won't download again."
-    return
-  end
-
   url = "https://covers.openlibrary.org/b/id/#{cover_id}-M.jpg"
-  puts "Downloading cover image for book '#{title}' to '#{file_path}'"
+  puts "Downloading cover image for book '#{title}' to '#{cover_image_path}'"
   download = open(url)
-  IO.copy_stream(download, file_path)
+  IO.copy_stream(download, cover_image_path)
 end
-
-count = 0
 
 outline.each do |chapter|
   books = chapter['books']
   if books
     books.each do |book|
-      count += 1
       fetch_book_info(book['title'], book['subtitle'], book['author'])
-
-      if count >= 10
-        raise "Exiting at max count"
-      end
     end
   end
 end
