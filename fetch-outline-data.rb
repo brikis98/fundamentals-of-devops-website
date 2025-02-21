8aad72404bd2cd190dd48171418a09caba4772f0
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2025-02-21 18:07:42 +0000

    Initial version of fetching image and description for other resources

--- fetch-outline-data.rb
+++ fetch-outline-data.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'open-uri'
 require 'nokogiri'
 
-def fetch_description_from_open_library(title, subtitle, author)
+def fetch_book_description_from_open_library(title, subtitle, author)
   query = URI.encode_www_form_component("#{title} #{subtitle || author}")
   base_url = "https://openlibrary.org/search.json?q=#{query}&limit=1"
   response = make_http_request_with_retries(base_url)
@@ -28,7 +28,7 @@ def make_http_request_with_retries(url)
   retries = 0
 
   begin
-    URI.open(url)
+    URI.open(url, "User-Agent" => "Mozilla/5.0")
   rescue => error
     puts "WARN: Got error while calling URL '#{url}': '#{error}'"
     retries += 1
@@ -42,7 +42,7 @@ def make_http_request_with_retries(url)
   end
 end
 
-def fetch_description_from_goodreads(title, subtitle, author)
+def fetch_book_description_from_goodreads(title, subtitle, author)
   query = URI.encode_www_form_component("#{title} #{subtitle || author}")
   search_url = "https://www.goodreads.com/search?q=#{query}"
   doc = Nokogiri::HTML(make_http_request_with_retries(search_url))
@@ -127,8 +127,8 @@ def download_cover(title, cover_id, cover_image_path)
 end
 
 # Ruby has no way to update YAML without completely changing the formatting, so to avoid turning outline.yaml into a
-# hot mess, this is a hacky method that uses regex to insert YAML elements into the book field of the YAML.
-def add_element_to_book_yaml(title, outline_as_str, element_name, element_value)
+# hot mess, this is a hacky method that uses regex to insert YAML elements.
+def add_element_to_outline_yaml(title, outline_as_str, element_name, element_value)
   unless element_value
     puts "WARN: did not find a '#{element_name}' for '#{title}'"
     return outline_as_str
@@ -136,42 +136,104 @@ def add_element_to_book_yaml(title, outline_as_str, element_name, element_value)
 
   puts "Adding '#{element_name}' to YAML for '#{title}'"
   element_value = element_value.gsub('"', "'").gsub("\n", " ")
-  outline_as_str.gsub(/^(    - title: "#{title}")/, "\\1\n      #{element_name}: \"#{element_value}\"")
+  outline_as_str.gsub(/^(    - title: "#{Regexp.escape(title)}")/, "\\1\n      #{element_name}: \"#{element_value}\"")
 end
 
-def process_outline(outline, outline_as_str, max_to_process)
-  count = 0
-  outline.each do |chapter|
-    books = chapter['books']
-    if books
-      books.each do |book|
-        count += 1
+def process_books(chapter, outline_as_str)
+  books = chapter['books']
+  unless books
+    return outline_as_str
+  end
 
-        title = book['title']
-        subtitle = book['subtitle']
-        author = book['author']
-        image = book['image']
-        description = book['description']
+  books.each do |book|
+    title = book['title']
+    subtitle = book['subtitle']
+    author = book['author']
+    image = book['image']
+    description = book['description']
 
-        if description
-          puts "Book '#{title}' already has a description. Will not try to update it."
-        else
-          description = fetch_description_from_open_library(title, subtitle, author) || fetch_description_from_goodreads(title, subtitle, author)
-          outline_as_str = add_element_to_book_yaml(title, outline_as_str, 'description', description)
-        end
+    if description
+      puts "Book '#{title}' already has a description. Will not try to update it."
+    else
+      description = fetch_book_description_from_open_library(title, subtitle, author) || fetch_book_description_from_goodreads(title, subtitle, author)
+      outline_as_str = add_element_to_outline_yaml(title, outline_as_str, 'description', description)
+    end
 
-        if image
-          puts "Book '#{title}' already has an image. Will not try to update it."
-        else
-          image_file_path = fetch_book_cover_image_from_open_library(title, author, description)
-          outline_as_str = add_element_to_book_yaml(title, outline_as_str, 'image', image_file_path)
-        end
+    if image
+      puts "Book '#{title}' already has an image. Will not try to update it."
+    else
+      image_file_path = fetch_book_cover_image_from_open_library(title, author, description)
+      outline_as_str = add_element_to_outline_yaml(title, outline_as_str, 'image', image_file_path)
+    end
+  end
 
-        if max_to_process && count >= max_to_process
-          puts "Hit max count of #{max_to_process}, so will not process the outline further."
-          return outline_as_str
-        end
+  outline_as_str
+end
+
+def extract_description_from_doc(doc)
+  doc.at('meta[name="description"]')&.[]('content') || doc.at('meta[property="og:description"]')&.[]('content')
+end
+
+def fetch_image_for_doc(doc, url)
+  if url.include?("youtube.com") || url.include?("youtu.be")
+    video_id = url[/v=([^&]+)/, 1] || url.split('/').last
+    "https://img.youtube.com/vi/#{video_id}/hqdefault.jpg"
+  elsif url.include?("vimeo.com")
+    video_id = url.split('/').last
+    vimeo_api_url = "https://vimeo.com/api/v2/video/#{video_id}.json"
+    json_data = JSON.parse(make_http_request_with_retries(vimeo_api_url).read)
+    json_data.first["thumbnail_large"]
+  else
+    doc.at('meta[property="og:image"]')&.[]('content')
+  end
+end
+
+def process_other_resources(chapter, outline_as_str)
+  other_resources = chapter['other_resources']
+  unless other_resources
+    return outline_as_str
+  end
+
+  other_resources.each do |other_resource|
+    title = other_resource['title']
+    image = other_resource['image']
+    description = other_resource['description']
+    url = other_resource['url']
+
+    unless description && image
+      html = make_http_request_with_retries(url).read
+      doc = Nokogiri::HTML(html)
+
+      if description
+        puts "Other resource '#{title}' already has a description. Will not try to update it."
+      else
+        description = extract_description_from_doc(doc)
+        outline_as_str = add_element_to_outline_yaml(title, outline_as_str, 'description', description)
       end
+
+      if image
+        puts "Other resource '#{title}' already has an image. Will not try to update it."
+      else
+        image_file_path = fetch_image_for_doc(doc, url)
+        outline_as_str = add_element_to_outline_yaml(title, outline_as_str, 'image', image_file_path)
+      end
+    end
+  end
+
+  outline_as_str
+end
+
+def process_outline(outline, outline_as_str, max_chapters_to_process)
+  chapters_processed = 0
+  outline.each do |chapter|
+    outline_as_str = process_books(chapter, outline_as_str)
+    outline_as_str = process_other_resources(chapter, outline_as_str)
+
+    chapters_processed +=1
+
+    if chapters_processed >= max_chapters_to_process
+      puts "Processed max allowed chapters (#{max_chapters_to_process}). Will not process any more."
+      return outline_as_str
     end
   end
 
@@ -182,9 +244,9 @@ outline_file_path = '_data/outline.yml'
 outline = YAML.load_file(outline_file_path)
 outline_as_str = File.read(outline_file_path)
 
-# Set to nil to process all books
-max_to_process = nil
+# Set to nil to process all chapters
+max_chapters_to_process = 1
 
-updated_outline_as_str = process_outline(outline, outline_as_str, max_to_process)
+updated_outline_as_str = process_outline(outline, outline_as_str, max_chapters_to_process)
 puts "Updating '#{outline_file_path}'"
 File.write(outline_file_path, updated_outline_as_str)
